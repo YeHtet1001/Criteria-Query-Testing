a8c5444ba6d3d565d5f6d6e6130d43e77918569a
LENOVO <LENOVO@DESKTOP-45AODF4>
2025-01-24 11:39:35 +0630

    testing

--- revision-dynamic-query-baserepository/src/main/java/com/yha/model/repo/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.yha.model.repo;
-
-import com.yha.model.BaseRepository;
-import com.yha.model.entity.Student;
-
-public interface StudentRepo extends BaseRepository< Student , Long > {
-
-}
